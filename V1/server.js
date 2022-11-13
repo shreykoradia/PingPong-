01c98299cd641fb379ff287fe452fa61c584a688
shreykoradia <shrey.koradia@yahoo.com>
2022-11-13 22:32:24 +0530

    done some minor changes

--- V1/server.js
+++ V1/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const http = require("http");
 const PORT  = process.env.PORT  || 3002
 const socketio = require("socket.io");
+const { getUsersInRoom } = require('./user');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -17,5 +18,23 @@ server.listen(PORT , ()=>{
 
 // handling the socket connection 
 io.on('connection' , (socket)=>{
-    // console.log("New Connection Found ")
+    socket.on('join',(playload , callback)=>{
+        let numberOfUsersInRoom = getUsersInRoom(playload.room).length 
+
+        const {error , newUser} = addUser({
+            id : socket.id,
+            name : numberOfUsersInRoom == 0 ? Player1 : Player2,
+            room : playload.room
+        }) 
+
+        if(error)
+        return callback(error)
+
+        socket.join(newUser.room)
+
+        io.to(newUser.room).emit('roomData', {room: newUser.room, users: getUsersInRoom(newUser.room)})
+        socket.emit('currentUserData', {name: newUser.name})
+        callback()
+
+    })
 })
